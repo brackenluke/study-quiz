4cf37903b12e0d2704345e6e7d70e57c406da811
brackenluke <156158313+brackenluke@users.noreply.github.com>
2024-03-05 00:42:47 -0600

    Added Study Quiz questions and answers to javascript file.

---
+++ assets/script.js
@@ -0,0 +1,67 @@
+// script.js
+
+// Study Quiz questions and Answers
+
+let studyQuestions = [
+    {
+        prompt: "<h1>Text</h1> is the correct way of making a header in HTML.",
+        options: [
+            "True",
+            "False",
+        ],
+        answer: "True"
+    },
+
+    {
+        prompt: "What does HTML stand for?",
+        options: [
+            "Hyper Text Markup Leveler",
+            "Hyper Text Marketing Language",
+            "Hyper Texter Marker Lingo",
+            "Hyper Text Markup Language",
+        ],
+        answer: "Hyper Text Markup Language",
+    },
+
+    {
+        prompt: "What does CSS stand for?",
+        options: [
+            "Cascading Style Sheets",
+            "Colorful Style Sheets",
+            "Computing Style Sheets",
+            "Cotton Super Street",
+        ],
+        answer: "Cascading Style Sheets",
+    },
+
+    {
+        prompt: "Where in the HTML document is the correct place to refer to an external style sheet?",
+        options: [
+            "In the <body> section",
+            "In the <head> section",
+            "At the end of the document",
+        ],
+        answer: "In the <head> section",
+    },
+
+    {
+        prompt: "Where is the correct place to insert a JavaScript?",
+        options: [
+            "The <body> section",
+            "The <head> section",
+            "Both the <head> section and the <body> section are correct",
+        ],
+        answer: "Both the <head> section and the <body> section are correct",
+    },
+
+    {
+        prompt: "What is the correct syntax for refferring to an external script called 'xyz.js'?",
+        options: [
+            "<script src='xyz.js'>",
+            "<script href='xyz.js'>",
+            "<script class='xyz.js'>",
+        ],
+        answer: "<script src='xyz.js'>",
+    },
+];
+
